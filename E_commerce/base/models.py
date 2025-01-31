5e3ebdb1e29dac55b4f38cfdab5285c6af70b689
JakirNobi <jakirnoby@gmail.com>
2025-01-31 22:07:09 +0600

    Product model added

--- E_commerce/base/models.py
+++ E_commerce/base/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+CATEGORY_CHOICES =(
+    ('EB','Earbuds'),
+    ('SW','Smart Watch'),
+    ('MS','Mouse'),
+    ('SB','Sound Box'),
+    ('RT','Router'),
+    ('SF','Stand Fan'),
+    ('PT','Printer'),
+    ('KB','Keyboard'),
+    ('HP','Headphone'),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products')
+    def __str__(self): 
+        return self.title
