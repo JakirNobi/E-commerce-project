e58aa8768ee873d01f9b693db88fb6e0fd44d6f0
JakirNobi <jakirnoby@gmail.com>
2025-02-01 20:01:00 +0600

    hjkl

--- E_commerce/manage.py
+++ E_commerce/manage.py
@@ -4,25 +4,9 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_commerce.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
-
-# if __name__ == "__main__":
-#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "E_commerce.settings")
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'E_commerce.settings')
 #     try:
 #         from django.core.management import execute_from_command_line
 #     except ImportError as exc:
@@ -31,6 +15,22 @@ if __name__ == '__main__':
 #             "available on your PYTHONPATH environment variable? Did you "
 #             "forget to activate a virtual environment?"
 #         ) from exc
-#     # Bind to the port from the environment variable, defaulting to 8000
-#     port = os.getenv('PORT', '8000')
-#     execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "E_commerce.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    # Bind to the port from the environment variable, defaulting to 8000
+    port = os.getenv('PORT', '8000')
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
