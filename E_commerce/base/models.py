3bad6ed9a82a09e400186a9d0692ed0e3d850252
JakirNobi <jakirnoby@gmail.com>
2025-02-01 19:47:03 +0600

    h

--- E_commerce/base/models.py
+++ E_commerce/base/models.py
@@ -25,3 +25,4 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products')
     def __str__(self): 
         return self.title
+    
--- E_commerce/base/views.py
+++ E_commerce/base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views import View 
+from .models import Product
 
 # Create your views here.
 def home(request):
     return render (request,"base/index.html")
+
+class CategoryView(View):
+    def get(self,request):
+        products =Product.objects.all()
+        return render(request,"base/category.html", {"products":products})
