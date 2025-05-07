1a2cf6738274359dd45ff11893844756a8b328f6
JakirNobi <jakirnoby@gmail.com>
2025-05-07 21:23:03 +0600

    hmm

--- E_commerce/user/models.py
+++ E_commerce/user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100,blank=True,null=True)
+    address = models.CharField(max_length=200,blank=True,null=True)
+    country = models.CharField(max_length=100,blank=True,null=True)
+    city = models.CharField(max_length=100,blank=True,null=True)
+    zip_code = models.CharField(max_length=20, blank=True,null=True)
+    phone_number = models.CharField(max_length=20, blank=True,null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
