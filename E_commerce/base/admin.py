ae5efe032ecf03105c28d5c7bfa1f12a77887354
JakirNobi <jakirnoby@gmail.com>
2025-05-10 16:24:47 +0600

    product gallery

--- E_commerce/base/admin.py
+++ E_commerce/base/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from base.models import Category,Product
+from base.models import Category,Product,ProductImage
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
 
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
     prepopulated_fields ={'slug':('name',)}
+
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product,ProductAdmin)
--- E_commerce/base/views.py
+++ E_commerce/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View 
 from django.views.generic import ListView, DetailView
-from base.models import Product, Category
+from base.models import Product, Category,ProductImage
 # Create your views here.
 class HomeView(ListView):
     model = Product
@@ -10,10 +10,15 @@ class HomeView(ListView):
 
 class CategoryView(ListView):
     model = Product
-    template_name = "base/category.html"
+    template_name = "base/all_products.html"
     context_object_name = 'all'
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = "base/product.html"
     context_object_name = 'item'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_images'] = ProductImage.objects.filter(product=self.object.id)
+        return context
