912ea8eda0363464070f19c2d41024bf82f29811
JakirNobi <jakirnoby@gmail.com>
2025-05-11 04:17:41 +0600

    cart function

--- E_commerce/order/urls.py
+++ E_commerce/order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from order import views
 app_name = 'order'
 urlpatterns = [
-    
+    path('add_to_cart/<int:pk>/',views.add_to_cart,name='add-to-cart'),
 ]
--- E_commerce/user/urls.py
+++ E_commerce/user/urls.py
@@ -7,7 +7,7 @@
 
 from django.urls import path
 from . import views
-
+app_name = 'user'
 urlpatterns = [
     path('', views.login_signup_view, name='login_signup'),
     path('logout/', views.logout_view, name='logout'),
