79bc8b1e09cb3df864cc040002262243ae500bad
JakirNobi <jakirnoby@gmail.com>
2025-05-17 21:23:03 +0600

    empty cart

--- E_commerce/order/urls.py
+++ E_commerce/order/urls.py
@@ -6,4 +6,5 @@ app_name = 'order'
 urlpatterns = [
     path('add_to_cart/<int:pk>/',views.add_to_cart,name='add-to-cart'),
     path('cart_view/',views.cart_view,name='cart'),
+    path('remove_item/<int:pk>/',views.remove_item_from_cart,name='remove-item')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
