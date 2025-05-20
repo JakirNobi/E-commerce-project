665262b500dbd1f79467e1ce61a64a787f776a3c
JakirNobi <jakirnoby@gmail.com>
2025-05-20 11:53:55 +0600

    cart increment and decrement

--- E_commerce/order/views.py
+++ E_commerce/order/views.py
@@ -60,3 +60,39 @@ def remove_item_from_cart(request, pk):
             return redirect('order:cart')
     else:
         return redirect('order:cart')
+    
+def increase_cart(request,pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user,ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item,user=request.user,purchased=False)[0]
+            if order_item.quantity >= 1:
+                order_item.quantity += 1
+                order_item.save()
+                return redirect('order:cart')
+        else:
+            return redirect('base:home')
+    else:
+        return redirect('base:home')
+    
+def decrease_cart(request,pk):
+    item = get_object_or_404(Product,pk=pk)
+    order_qs = Order.objects.filter(user=request.user,ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item,user=request.user,purchased=False)[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+                return redirect('order:cart')
+            else:
+                order.orderitems.remove(order_item)
+                order_item.delete()
+                return redirect ('order:cart')
+        else:
+            return redirect('base:home')
+    else:
+        return redirect('base:home')
