cab0a011a0f9ecbcca91a9f328a7c26ed0506e37
JakirNobi <jakirnoby@gmail.com>
2025-05-20 21:49:16 +0600

    multi page all products

--- E_commerce/base/views.py
+++ E_commerce/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View 
 from django.views.generic import ListView, DetailView
 from base.models import Product, Category,ProductImage
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 class HomeView(ListView):
     model = Product
@@ -12,6 +13,7 @@ class AllProductsView(ListView):
     model = Product
     template_name = "base/all_products.html"
     context_object_name = 'all'
+    paginate_by = 8
 
 class ProductDetailView(DetailView):
     model = Product
