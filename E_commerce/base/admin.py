739d126f984ae885fd1289aefad87cf11b80b0ea
JakirNobi <jakirnoby@gmail.com>
2025-06-20 17:58:44 +0600

    profile view

--- E_commerce/base/admin.py
+++ E_commerce/base/admin.py
@@ -1,16 +1,21 @@
+from base.models import Category, Product, ProductImage
 from django.contrib import admin
-from base.models import Category,Product,ProductImage
+
 
 class ProductImageAdmin(admin.StackedInline):
     model = ProductImage
 
+
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin]
-    prepopulated_fields ={'slug':('name',)}
+    prepopulated_fields = {"slug": ("name",)}
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields={'slug':('name',)}
+    prepopulated_fields = {"slug": ("name",)}
+
 
 # Register your models here.
-admin.site.register(Category,CategoryAdmin)
-admin.site.register(Product,ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
